4719cfbba056d195dcdb8ec346c9aca34f2ea4b6
Schaidt Patriota <schaidt534@gmail.com>
2023-08-02 19:48:59 -0300

    Created Project

---
+++ src/main/java/br/com/draftpad/DraftPadApplication.java
@@ -0,0 +1,13 @@
+package br.com.draftpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftPadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/draftpad/DraftPadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.draftpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
